7f7902e5119442645303c30ef4f7bfb8fd6910e5
Sue Roh Windows <sueroh@gmail.com>
2020-11-08 20:08:47 -0500

    Added map directions

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,3 +1,93 @@
+let userCoords = {
+	lat: 0,
+	long: 0
+}
+
+let map
+let MAPBOX_TOKEN = 
+
+// Get user geolocation
+function successCallback (data) {
+	userCoords.lat = data.coords.latitude
+	userCoords.long = data.coords.longitude
+	getDirections()
+
+}
+
+function errorCallback (err) {
+	console.log(err)
+}
+
+(function getUserLocation() {
+	navigator.geolocation.getCurrentPosition(successCallback, errorCallback, {
+		maximumAge: Infinity
+	})
+})();
+
+
+// MapboxGL map
+(function createMap() {
+	let mapEl = document.createElement('div')
+	mapEl.id = 'serverMap'
+	document.body.appendChild(mapEl)
+
+	mapboxgl.accessToken = MAPBOX_TOKEN
+	map = new mapboxgl.Map({
+		container: 'serverMap', // container id
+		style: 'mapbox://styles/wiseshrimp/cjhv5jyh409l62rmzwvwckjzl', // style URL
+		center: [-97.317, 38.105], // starting position [lng, lat]
+		zoom: 4.58 // starting zoom
+	})
+})();
+
+let getDirections = () => {
+	if (userCoords.lat && userCoords.long) {
+		let url = `https://api.mapbox.com/directions/v5/mapbox/driving/${userCoords.long},${userCoords.lat};-122.486052,37.830348?access_token=${MAPBOX_TOKEN}`
+		$.get(url, data => {
+			if (data.routes.length) {
+				let coords = polyline.toGeoJSON(data.routes[0].geometry)
+
+				map.addSource('route', {
+					'type': 'geojson',
+					'data': {
+						'type': 'Feature',
+						'properties': {},
+						'geometry': {
+							'type': 'LineString',
+							'coordinates': coords.coordinates
+						}
+					}
+				})
+			} else {
+				map.addSource('route', {
+					'type': 'geojson',
+					'data': {
+						'type': 'Feature',
+						'properties': {},
+						'geometry': {
+							'type': 'LineString',
+							'coordinates': [] // Add coords from urls
+						}
+					}
+				})
+			}
+			map.addLayer({
+				'id': 'route',
+				'type': 'line',
+				'source': 'route',
+				'layout': {
+					'line-join': 'round',
+					'line-cap': 'round'
+				},
+				'paint': {
+					'line-color': '#FE4473',
+					'line-width': 5
+				}
+			})
+		})
+	}
+}
+
 chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "complete") {
@@ -5,7 +95,6 @@ chrome.extension.sendMessage({}, function(response) {
 
 		// ----------------------------------------------------------
 		// This part of the script triggers when page is done loading
-		// alert("hello!!!!!!")
 		// ----------------------------------------------------------
 
 		// Check performance support
@@ -20,9 +109,8 @@ chrome.extension.sendMessage({}, function(response) {
 			console.log("there are NO `resource` performance records")
 			return
 		}
-	
 		for (let i = 0; i < resources.length; i++) {
-			console.log(resources[i].name) // these are urls of requests
+			// console.log(resources[i].name) // these are urls of requests
 		}
 	}
 	}, 10)
